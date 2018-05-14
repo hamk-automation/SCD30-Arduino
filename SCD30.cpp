5779cb45e75592db506dbbfbfe2d64948c3fdf88
Minh-Khoa Dang <minhkhoadang@gmail.com>
2018-05-14 10:42:52 +0300

    init

---
+++ SCD30.cpp
@@ -0,0 +1,153 @@
+/*!
+ * @file SCD30.cpp
+ *
+ * @mainpage Adafruit SCD30 gas sensor driver
+ *
+ * @section intro_sec Introduction
+ *
+ * This is the documentation for Adafruit's SCD30 driver for the
+ * Arduino platform.  It is designed specifically to work with the
+ * Adafruit SCD30 breakout: http://www.adafruit.com/products/3709
+ *
+ * These sensors use I2C to communicate, 2 pins (SCL+SDA) are required
+ * to interface with the breakout.
+ *
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ *
+ * @section author Author
+ * Written by Ladyada for Adafruit Industries.
+ *
+ * @section license License
+ * BSD license, all text here must be included in any redistribution.
+ *
+ */
+
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include "SCD30.h"
+
+//#define I2C_DEBUG
+
+/**************************************************************************/
+/*! 
+    @brief  Instantiates a new SCD30 class
+*/
+/**************************************************************************/
+SCD30::SCD30() {
+}
+
+/**************************************************************************/
+/*! 
+    @brief  Setups the hardware and detects a valid SCD30. Initializes I2C
+    then reads the serialnumber and checks that we are talking to an SCD30
+    @param  theWire Optional pointer to I2C interface, otherwise use Wire
+    @returns True if SCD30 found on I2C, False if something went wrong!
+*/
+/**************************************************************************/
+boolean SCD30::begin(TwoWire *theWire) {
+  _i2caddr = SCD30_I2CADDR_DEFAULT;
+  if (theWire == NULL) {
+    _i2c = &Wire;
+  } else {
+    _i2c = theWire;
+  }
+
+  _i2c->begin();
+
+  return true;
+}
+
+
+
+/**************************************************************************/
+/*! 
+    @brief  I2C low level interfacing
+*/
+/**************************************************************************/
+
+
+boolean SCD30::readWordFromCommand(uint8_t command[], uint8_t commandLength, 
+                                  uint16_t delayms, uint16_t *readdata, uint8_t readlen)
+{
+  uint8_t data;
+
+  _i2c->beginTransmission(_i2caddr);
+
+#ifdef I2C_DEBUG
+  Serial.print("\t\t-> ");
+#endif
+
+  for (uint8_t i=0; i<commandLength; i++) {
+    _i2c->write(command[i]);
+#ifdef I2C_DEBUG
+    Serial.print("0x"); Serial.print(command[i], HEX); Serial.print(", ");
+#endif
+  }
+#ifdef I2C_DEBUG
+  Serial.println();
+#endif
+  _i2c->endTransmission();
+  
+  if (readlen == 0) 
+    return true;
+
+  uint8_t replylen = readlen * (SCD30_WORD_LEN +1);
+  if (_i2c->requestFrom(_i2caddr, replylen) != replylen) 
+    return false;
+  uint8_t replybuffer[replylen];
+#ifdef I2C_DEBUG
+  Serial.print("\t\t<- ");
+#endif  
+  for (uint8_t i=0; i<replylen; i++) {
+    replybuffer[i] = _i2c->read();
+#ifdef I2C_DEBUG
+    Serial.print("0x"); Serial.print(replybuffer[i], HEX); Serial.print(", ");
+#endif
+  }
+
+#ifdef I2C_DEBUG
+  Serial.println();
+#endif
+
+  for (uint8_t i=0; i<readlen; i++) {
+    uint8_t crc = generateCRC(replybuffer+i*3, 2);
+#ifdef I2C_DEBUG
+    Serial.print("\t\tCRC calced: 0x"); Serial.print(crc, HEX);
+    Serial.print(" vs. 0x"); Serial.println(replybuffer[i * 3 + 2], HEX);
+#endif
+    if (crc != replybuffer[i * 3 + 2])
+      return false;
+    // success! store it
+    readdata[i] = replybuffer[i*3];
+    readdata[i] <<= 8;
+    readdata[i] |= replybuffer[i*3 + 1];
+#ifdef I2C_DEBUG
+    Serial.print("\t\tRead: 0x"); Serial.println(readdata[i], HEX);
+#endif
+  }
+  return true;
+}
+
+uint8_t SCD30::generateCRC(uint8_t *data, uint8_t datalen) {
+  // calculates 8-Bit checksum with given polynomial
+  uint8_t crc = SCD30_CRC8_INIT;
+
+  for (uint8_t i=0; i<datalen; i++) {
+    crc ^= data[i];
+    for (uint8_t b=0; b<8; b++) {
+      if (crc & 0x80)
+	crc = (crc << 1) ^ SCD30_CRC8_POLYNOMIAL;
+      else
+	crc <<= 1;
+    }
+  }
+  return crc;
+}
---
+++ SCD30.h
@@ -0,0 +1,45 @@
+/*!
+
+*/
+
+#include "Arduino.h"
+#include <Wire.h>
+
+// the i2c address
+#define SCD30_I2CADDR_DEFAULT 0x61     ///< SGP30 has only one I2C address
+
+// commands and constants
+#define SCD30_CRC8_POLYNOMIAL  0x31    ///< Seed for SGP30's CRC polynomial
+#define SCD30_CRC8_INIT        0xFF    ///< Init value for CRC
+#define SCD30_WORD_LEN         2       ///< 2 bytes per word
+
+/**************************************************************************/
+/*!  Class that stores state and functions for interacting with SGP30 Gas Sensor */
+/**************************************************************************/
+class SCD30 {
+ public:
+  SCD30();
+  boolean begin(TwoWire *theWire = NULL);
+  float fTemp, fRH, fCO2;
+
+  boolean trigSingleMeasurement(uint16_t ambientPressure = 0);
+  boolean trigContinuousMeasurement(uint16_t ambientPressure = 0);
+  boolean stopContinuousMeasurement();
+  boolean setMeasurementInterval(uint16_t uInterval = 2);
+  boolean getDataReadyStatus();
+  boolean readMeasurement();
+  boolean setTemperatureOffset(uint16_t tempOffset);
+  boolean setAltitudeOffset(uint16_t altitude);
+
+  boolean setASC(boolean enable = true);
+  boolean setFRCValue(uint16_t co2baseline);
+
+ private:
+  TwoWire *_i2c;
+  uint8_t _i2caddr;
+
+  void write(uint8_t address, uint8_t *data, uint8_t n);
+  void read(uint8_t address, uint8_t *data, uint8_t n);
+  boolean readWordFromCommand(uint8_t command[], uint8_t commandLength, uint16_t delay, uint16_t *readdata = NULL, uint8_t readlen = 0);
+  uint8_t generateCRC(uint8_t data[], uint8_t datalen);
+};
